f4ae5b9e5fa23adbc9452bd40efbe9498af74c8e
Ilona Tatarenko <ilona-tatarenko@mail.ru>
2022-10-07 01:20:12 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+int price = 1000;
+int amount = 20;
+int mili = price/amount;
+
+
+
+System.out.println("начисленные мили " + mili);
+
+
+    }
+}
