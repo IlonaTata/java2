1d457a2b47cabb17d7f80ed8247194f35a5cf0d8
Ilona Tatarenko <ilona-tatarenko@mail.ru>
2022-10-15 19:53:21 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,12 @@ public class Main {
     public static void main(String[] args) {
 
 
-int price = 1000;
-int amount = 20;
-int mili = price/amount;
+        int price = 1000;
+        int amount = 20;
+        int mili = price / amount;
 
 
-
-System.out.println("начисленные мили " + mili);
+        System.out.println("начисленные мили " + mili);
 
 
     }
